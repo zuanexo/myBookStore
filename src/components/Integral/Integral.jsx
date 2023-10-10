f6e05fa1d559e9d98f3dbea5bfe01f43b8f72a14
zuanexo <grooovydon@gmail.com>
2023-10-10 20:50:23 +0530

    js

---
+++ src/components/Integral/Integral.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+
+export default function Integral() {
+  return (
+    <div className="integral">
+      <div className="integral-left">
+        <div className="integral-logo">logo here</div>
+        <div className="userCard">
+          <div className="userCard-pic"></div>
+          <div className="userCard-right">
+            <div className="userCard-name">Aman</div>
+            <div className="userCard-email">Aman@aasdasd</div>
+          </div>
+        </div>
+        <div className="sideLinks">
+          <div className="sideLinks-card">
+            <div className="sideLinks-icon"></div>
+            <div className="sideLinks-text">asasdasdasd</div>
+          </div>
+          <div className="sideLinks-card">
+            <div className="sideLinks-icon"></div>
+            <div className="sideLinks-text">asasdasdasd</div>
+          </div>
+          <div className="sideLinks-card">
+            <div className="sideLinks-icon"></div>
+            <div className="sideLinks-text">asasdasdasd</div>
+          </div>
+          <div className="sideLinks-card">
+            <div className="sideLinks-icon"></div>
+            <div className="sideLinks-text">asasdasdasd</div>
+          </div>
+          <div className="sideLinks-card">
+            <div className="sideLinks-icon"></div>
+            <div className="sideLinks-text">asasdasdasd</div>
+          </div>
+          <div className="sideLinks-card">
+            <div className="sideLinks-icon"></div>
+            <div className="sideLinks-text">asasdasdasd</div>
+          </div>
+        </div>
+      </div>
+      Integral
+    </div>
+  )
+}
